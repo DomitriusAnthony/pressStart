a92b8803bea73d5976f38c07a61398ad6c25a539
Domitrius Clark <domitrius.anthony@hotmail.com>
2017-03-30 02:43:53 -0400

    Styled account details form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		@feed = Post.feed(@user)
+    @users = User.all
+    @post = Post.find(params[:id])
+    @posts = Post.all
+
   	end
 
   	def follow
