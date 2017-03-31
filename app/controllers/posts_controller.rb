133c525611b5130c31a0544839feab84d9d25356
Domitrius Clark <domitrius.anthony@hotmail.com>
2017-03-31 00:20:05 -0400

    Color styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,9 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     session[:conversations] ||= []
-    @conversations = Conversation.includes(:recipient, :messages).find(session[:conversations])
     @users = User.all.where.not(id: current_user)
+    @conversations = Conversation.includes(:recipient, :messages).find(session[:conversations])
+  
 
     @post = Post.new
     @posts = Post.all
