b8f68a2f6e58d49514957a965e9e642e5c3e1c98
Domitrius Clark <domitrius.anthony@hotmail.com>
2017-04-02 17:45:13 -0400

    Fixed inbox to only show users you follow

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -4,6 +4,7 @@ class InboxController < ApplicationController
 
 		@users = User.all.where.not(id: current_user)
 		@conversations = Conversation.includes(:recipient, :messages).find(session[:conversations])
+		@followed = current_user.followed if signed_in?
 	end
 
 	
